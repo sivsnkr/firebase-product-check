8bce8956ab6f937c53af86ee33a8f3faffb31128
ced17i026 <ced17i026@iiitdm.ac.in>
2019-03-21 21:22:33 +0530

    add firebase to the project

--- server/app.js
+++ server/app.js
@@ -8,3 +8,14 @@ const app = express();
 
 //use
 app.use(express.json());
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDR2nW4QtGnEfcVTTkp8o4ryFiGY8ztLo4",
+    authDomain: "fir-product-check.firebaseapp.com",
+    databaseURL: "https://fir-product-check.firebaseio.com",
+    projectId: "fir-product-check",
+    storageBucket: "fir-product-check.appspot.com",
+    messagingSenderId: "803024683883"
+};
+firebase.initializeApp(config);
