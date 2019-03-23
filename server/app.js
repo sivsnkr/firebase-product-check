c1afc5a14dbb7fff071dbc213d46b60088add00b
ced17i026 <ced17i026@iiitdm.ac.in>
2019-03-23 18:04:18 +0530

    add firebase auth with email and password in app

--- server/app.js
+++ server/app.js
@@ -1,10 +1,8 @@
 const express = require("express"),
     firebase = require("firebase");
-
-
 //configuration
 const app = express();
-
+const port = process.env.port||3000;
 
 //use
 app.use(express.json());
@@ -19,3 +17,43 @@ var config = {
     messagingSenderId: "803024683883"
 };
 firebase.initializeApp(config);
+const auth = firebase.auth();
+
+//routes
+app.get('/',(req,res)=>{
+    const email = 'shiv@m.com';
+    const pass = "1235646465656";
+    //creating new user
+    auth.createUserWithEmailAndPassword(email, pass)
+    .then((createdUser)=>{
+        //
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+        console.log(error);
+    });
+
+    //signing in the created user
+    auth.signInWithEmailAndPassword(email, pass).
+    then((user)=>{
+        res.send("signing in user "+user);
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+        console.log(error);
+    });
+
+    //signing out user
+    auth.signOut().then(function() {
+        console.log("Sign-out successful");
+      }).catch(function(error) {
+        // An error happened.
+    });
+})
+
+//listening to server
+app.listen(port,()=>{console.log("Server is Running on "+port)});
